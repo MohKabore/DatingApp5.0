ed56dea2c9c1b59818c3c66917c582195ad7c600
mohKabore <mhkabore@gmail.com>
2021-12-16 18:08:56 +0000

    end of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,46 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
+using API.Dtos;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-       public class UsersController : BaseController
+    [Authorize]
+    public class UsersController : BaseController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         [Authorize]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return  await _context.Users.ToListAsync();
+            var users = await _userRepository.GetMembersAsync();
+         //   var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(users);
         }
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        public async Task<ActionResult<MemberDto>> GetUser(int id)
         {
-            return  await _context.Users.FirstOrDefaultAsync(user => user.Id==id);
+            var user = await _userRepository.GetUserByIdAsync(id);
+            var userToReturn = _mapper.Map<MemberDto>(user);
+            return Ok(userToReturn);
+        }
+        [HttpGet("getUser/{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
---
+++ API/Migrations/20211216141159_eodedez.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class eodedez : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBitrth",
+                table: "Users",
+                newName: "DateOfBirth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Users",
+                newName: "DateOfBitrth");
+        }
+    }
+}
